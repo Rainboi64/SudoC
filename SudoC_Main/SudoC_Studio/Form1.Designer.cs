f026a80585b3e8825586baf0f2675ad53a74450a
YamanAlhalabi <yaman.al.halabi1@gmail.com>
2020-04-06 01:52:24 +0300

    Added SudoC Studio.... just a source control

---
+++ SudoC_Main/SudoC_Studio/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace SudoC_Studio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ribbon1 = new System.Windows.Forms.Ribbon();
+            this.ribbonTab1 = new System.Windows.Forms.RibbonTab();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.documentMap1 = new FastColoredTextBoxNS.DocumentMap();
+            this.ruler1 = new FastColoredTextBoxNS.Ruler();
+            this.fastColoredTextBox2 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.documentMap2 = new FastColoredTextBoxNS.DocumentMap();
+            this.ruler2 = new FastColoredTextBoxNS.Ruler();
+            this.tAutoCompiler = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbon1
+            // 
+            this.ribbon1.CaptionBarVisible = false;
+            this.ribbon1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.ribbon1.Location = new System.Drawing.Point(0, 0);
+            this.ribbon1.Minimized = false;
+            this.ribbon1.Name = "ribbon1";
+            // 
+            // 
+            // 
+            this.ribbon1.OrbDropDown.BorderRoundness = 8;
+            this.ribbon1.OrbDropDown.Location = new System.Drawing.Point(0, 0);
+            this.ribbon1.OrbDropDown.Name = "";
+            this.ribbon1.OrbDropDown.Size = new System.Drawing.Size(527, 447);
+            this.ribbon1.OrbDropDown.TabIndex = 0;
+            this.ribbon1.OrbStyle = System.Windows.Forms.RibbonOrbStyle.Office_2013;
+            this.ribbon1.OrbText = "File";
+            this.ribbon1.RibbonTabFont = new System.Drawing.Font("Trebuchet MS", 9F);
+            this.ribbon1.Size = new System.Drawing.Size(1534, 146);
+            this.ribbon1.TabIndex = 0;
+            this.ribbon1.Tabs.Add(this.ribbonTab1);
+            this.ribbon1.TabsMargin = new System.Windows.Forms.Padding(5, 2, 20, 0);
+            this.ribbon1.TabSpacing = 4;
+            this.ribbon1.Text = "Home";
+            this.ribbon1.ThemeColor = System.Windows.Forms.RibbonTheme.Black;
+            // 
+            // ribbonTab1
+            // 
+            this.ribbonTab1.Name = "ribbonTab1";
+            this.ribbonTab1.Text = "Home";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 146);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.fastColoredTextBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.documentMap1);
+            this.splitContainer1.Panel1.Controls.Add(this.ruler1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.fastColoredTextBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.documentMap2);
+            this.splitContainer1.Panel2.Controls.Add(this.ruler2);
+            this.splitContainer1.Size = new System.Drawing.Size(1534, 512);
+            this.splitContainer1.SplitterDistance = 804;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(27, 14);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.CharHeight = 14;
+            this.fastColoredTextBox1.CharWidth = 8;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(658, 488);
+            this.fastColoredTextBox1.TabIndex = 3;
+            this.fastColoredTextBox1.Zoom = 100;
+            // 
+            // documentMap1
+            // 
+            this.documentMap1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.documentMap1.ForeColor = System.Drawing.Color.Maroon;
+            this.documentMap1.Location = new System.Drawing.Point(658, 24);
+            this.documentMap1.Name = "documentMap1";
+            this.documentMap1.Size = new System.Drawing.Size(146, 488);
+            this.documentMap1.TabIndex = 2;
+            this.documentMap1.Target = this.fastColoredTextBox1;
+            this.documentMap1.Text = "documentMap1";
+            // 
+            // ruler1
+            // 
+            this.ruler1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ruler1.Location = new System.Drawing.Point(0, 0);
+            this.ruler1.MaximumSize = new System.Drawing.Size(1073741824, 24);
+            this.ruler1.MinimumSize = new System.Drawing.Size(0, 24);
+            this.ruler1.Name = "ruler1";
+            this.ruler1.Size = new System.Drawing.Size(804, 24);
+            this.ruler1.TabIndex = 1;
+            this.ruler1.Target = this.fastColoredTextBox1;
+            // 
+            // fastColoredTextBox2
+            // 
+            this.fastColoredTextBox2.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox2.AutoScrollMinSize = new System.Drawing.Size(27, 14);
+            this.fastColoredTextBox2.BackBrush = null;
+            this.fastColoredTextBox2.CharHeight = 14;
+            this.fastColoredTextBox2.CharWidth = 8;
+            this.fastColoredTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox2.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fastColoredTextBox2.IsReplaceMode = false;
+            this.fastColoredTextBox2.Location = new System.Drawing.Point(0, 24);
+            this.fastColoredTextBox2.Name = "fastColoredTextBox2";
+            this.fastColoredTextBox2.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox2.ReadOnly = true;
+            this.fastColoredTextBox2.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox2.Size = new System.Drawing.Size(651, 488);
+            this.fastColoredTextBox2.TabIndex = 2;
+            this.fastColoredTextBox2.Zoom = 100;
+            // 
+            // documentMap2
+            // 
+            this.documentMap2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.documentMap2.ForeColor = System.Drawing.Color.Maroon;
+            this.documentMap2.Location = new System.Drawing.Point(651, 24);
+            this.documentMap2.Name = "documentMap2";
+            this.documentMap2.Size = new System.Drawing.Size(75, 488);
+            this.documentMap2.TabIndex = 1;
+            this.documentMap2.Target = this.fastColoredTextBox2;
+            this.documentMap2.Text = "documentMap2";
+            // 
+            // ruler2
+            // 
+            this.ruler2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ruler2.Location = new System.Drawing.Point(0, 0);
+            this.ruler2.MaximumSize = new System.Drawing.Size(1073741824, 24);
+            this.ruler2.MinimumSize = new System.Drawing.Size(0, 24);
+            this.ruler2.Name = "ruler2";
+            this.ruler2.Size = new System.Drawing.Size(726, 24);
+            this.ruler2.TabIndex = 0;
+            this.ruler2.Target = this.fastColoredTextBox2;
+            // 
+            // tAutoCompiler
+            // 
+            this.tAutoCompiler.Tick += new System.EventHandler(this.TAutoCompiler_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1534, 658);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.ribbon1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "SudoC Studio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Ribbon ribbon1;
+        private System.Windows.Forms.RibbonTab ribbonTab1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+        private FastColoredTextBoxNS.DocumentMap documentMap1;
+        private FastColoredTextBoxNS.Ruler ruler1;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox2;
+        private FastColoredTextBoxNS.DocumentMap documentMap2;
+        private FastColoredTextBoxNS.Ruler ruler2;
+        private System.Windows.Forms.Timer tAutoCompiler;
+    }
+}
+
---
+++ SudoC_Main/SudoC_Studio/Form1.cs
@@ -0,0 +1,44 @@
+﻿using SudoC_Main;
+using SudoC_Main.Compiler;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudoC_Studio
+{
+    public partial class Form1 :Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.Lua;
+            fastColoredTextBox2.Language = FastColoredTextBoxNS.Language.CSharp;
+            tAutoCompiler.Start();
+        }
+
+        private void TAutoCompiler_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Statics.dVars.Clear();
+                SudoC_Lexxer easyC_Lexxer = new SudoC_Lexxer();
+                sudoC_Assembler easyC_Assembler = new sudoC_Assembler();
+                fastColoredTextBox2.Text = easyC_Assembler.Assemble(easyC_Lexxer.Lex(fastColoredTextBox1.Text));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
